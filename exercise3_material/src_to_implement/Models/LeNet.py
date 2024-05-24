2d3a329ee83b308fddc427577326e64b7b31257a
Sri Ram Katta <sriram.katta@fau.de>
2024-05-24 22:00:58 +0200

    Lenet update

--- exercise3_material/src_to_implement/Models/LeNet.py
+++ exercise3_material/src_to_implement/Models/LeNet.py
@@ -1,12 +1,27 @@
-import pickle
+from Layers import Conv, FullyConnected, Flatten, ReLU, Pooling, SoftMax, Initializers
+from Optimization import Loss, Constraints, Optimizers
+import NeuralNetwork as nn
 
 def build():
-  pass
+  opti = Optimizers.Adam(5e-4,0.9,0.999)
+  opti.add_regularizer(Constraints.L2_Regularizer(4e-4))
+  net = nn.NeuralNetwork(opti, Initializers.He(), Initializers.He())
+  net.append_layer(Conv.Conv((1, 1), (6, 5, 5), 6))
+  net.append_layer(ReLU.ReLU())
+  net.append_layer(Pooling.Pooling((2, 2),(2, 2)))
+  net.append_layer(Conv.Conv((1, 1), (6, 5, 5), 16))
+  net.append_layer(ReLU.ReLU())
+  net.append_layer(Pooling.Pooling((2, 2), (2, 2)))
+  net.append_layer(Flatten.Flatten())
+  net.append_layer(FullyConnected.FullyConnected(16 * 7 * 7, 120))
+  net.append_layer(ReLU.ReLU())
+  net.append_layer(FullyConnected.FullyConnected(120, 84))
+  net.append_layer(ReLU.ReLU())
+  net.append_layer(FullyConnected.FullyConnected(84, 10))
+  net.append_layer(ReLU.ReLU())
+  net.append_layer(SoftMax.SoftMax())
+  net.loss_layer = Loss.CrossEntropyLoss()
+  return net
 
 
 
-def save(filename):
-  pass
-
-def load(filename, data_layer):
-  pass
