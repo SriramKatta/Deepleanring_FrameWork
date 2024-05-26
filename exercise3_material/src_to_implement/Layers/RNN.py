22ce905bf37ac7d5b691b257697058061c83178f
Sri Ram Katta <sriram.katta@fau.de>
2024-05-26 19:13:14 +0200

    RNN update

--- exercise3_material/src_to_implement/Layers/RNN.py
+++ exercise3_material/src_to_implement/Layers/RNN.py
@@ -1,20 +1,31 @@
 from Layers.Base import BaseLayer
+from Layers.FullyConnected import FullyConnected
+from Layers.Sigmoid import Sigmoid
+from Layers.TanH import TanH
+
+import numpy as np  
 
 class RNN(BaseLayer):
   def __init__(self, input_size, hidden_size, output_size):
     super().__init__()
     self.trainable = True
     self.input_size = input_size
-    self.output_size = output_size
     self.hidden_size = hidden_size
+    self.output_size = output_size
+    self.fc1 = FullyConnected(self.input_size + self.hidden_size, self.hidden_size)
+    self.fc2 = FullyConnected(self.hidden_size, self.output_size)
     self.initialize()
+    self.hidden_state = np.zeros((1, self.hidden_size))
+    self.grad_hidden_states = np.zeros((1, self.hidden_size))
     self.memval = False
     self.optimizerval = None
-
+    self.tanh = TanH()
+    self.sig = Sigmoid()
+    self.grad_weights = None
   
   def initialize(self, weights_initializer, bias_initializer):
-    self.weights = weights_initializer.initialize()
-    self.bias = bias_initializer.initialize()
+    self.fc1.initialize(weights_initializer, bias_initializer)
+    self.fc2.initialize(weights_initializer, bias_initializer)
 
   def forward(self, input_tensor):
     pass
