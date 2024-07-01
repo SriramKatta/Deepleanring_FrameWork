b5fd3484545b3926ac95fbcdcd72f0ff22d70880
Sriram katta <sriram.katta@fau.de>
2024-07-01 13:55:17 +0200

    RNN working

--- exercise3_material/src_to_implement/Layers/RNN.py
+++ exercise3_material/src_to_implement/Layers/RNN.py
@@ -1,106 +1,121 @@
 import numpy as np
-import copy
+import copy 
 from Layers import Base
-from Layers.FullyConnected import FullyConnected
-from Layers.TanH import TanH
-from Layers.Sigmoid import Sigmoid
+from Layers import Sigmoid
+from Layers import TanH
+from Layers import FullyConnected as FC
 
 
 class RNN(Base.BaseLayer):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.trainable = True
+        self.memorize_val = False
+        self.optimizer_val = None
+
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
-        self.optimizerval = None
-        self.tanhlay = TanH()
-        self.siglayer = Sigmoid()
-        self.fc_hidden = FullyConnected(hidden_size + input_size, hidden_size)
-        self.fc_out = FullyConnected(hidden_size, output_size)
-        self.h_t = None
-        self.h_t_prevbatch = None
-        self.y_t = None
-        self.memval = False
 
-    def forward(self, input_tensor):
-        self.time_size = input_tensor.shape[0]
-        output = np.zeros((self.time_size, self.output_size))
-        if self.memval == True:
-            if self.h_t is None:
-                self.h_t = np.zeros((self.time_size+1, self.hidden_size))
-            else :
-                self.h_t[0] = self.h_t_prevbatch
-        else:
-            self.h_t = np.zeros((self.time_size, self.output_size))
-
-        self.fc_hidden_input = []
-        self.tanh_activ = []
-        self.fc_out_input = []
-        self.siglayer_activ = []
-
-        for time in  range(self.time_size):
-            xt = input_tensor[time].reshape(1,-1)
-            hidd_t1 = self.h_t[time].reshape(1,-1)
-            fc_hidden_input = np.hstack((xt, hidd_t1))
-            
-            self.fc_hidden_input.append(np.array(fc_hidden_input))
-
-            self.hidden_state = self.tanhlay.forward(self.fc_hidden.forward(self.fc_hidden_input))
-            self.tanh_activ.append(np.array(self.hidden_state))
-
-            output[time] = self.siglayer.forward(self.fc_out.forward(self.hidden_state))
-            self.siglayer_activ.append(np.array(output[time]))
-
-        self.h_t_prevbatch = self.h_t[-1]
-        return np.array(output)
-    
-    def backward(self, error_tensor):
-        error_tensor_prev = np.zeros_like(self.input)
-        hidden_error = np.zeros((1, self.hidden_size))
-
-        for revtime in reversed(range(self.time_size)):
-            grad_o_t = self.siglayer.backward(error_tensor[revtime + 1].reshape((1,-1)))
-            self.fc_out.input_tensor_val = self.h_t[revtime + 1]
-            self.fc_out.backward(grad_o_t.reshape(1,-1))
-
-
-        return error_tensor_prev
+        self.hidden_state = [np.zeros(self.hidden_size)]
+        
+        self.fc_hidden = FC.FullyConnected(input_size+hidden_size, hidden_size)
+        self.tanh_lay = TanH.TanH()
+        self.sig_lay = Sigmoid.Sigmoid()
+        self.fc_out = FC.FullyConnected(hidden_size, output_size)
+        self.gradient_weights_val = None
+        self.gradient_weights_hy_val = None
+        self.weights = self.fc_hidden.weights
+        self.weights_hy = None
 
     def initialize(self, weights_initializer, bias_initializer):
         self.fc_hidden.initialize(weights_initializer, bias_initializer)
         self.fc_out.initialize(weights_initializer, bias_initializer)
+        self.weights = self.fc_hidden.weights
+        self.weights_hy = self.fc_out.weights
 
-    @property
-    def optimizer(self):
-        return self.optimizerval
+    def forward(self, input_tensor):
+        if self.memorize == False:
+            self.hidden_state = [np.zeros((1, self.hidden_size))]
 
-    @optimizer.setter
-    def optimizer(self, newval):
-        self.optimizerval = copy.deepcopy(newval)
+        self.input_tensor = input_tensor
+        self.batch_size = input_tensor.shape[0]
+        self.output = np.zeros((self.batch_size, self.output_size))
+        for time, intensor in enumerate(input_tensor):
+            x_t = intensor.reshape(1, -1)
+            h_t1 = self.hidden_state[-1].reshape(1, -1)
+            x_tilde = np.hstack([x_t, h_t1])
+            u_t = self.fc_hidden.forward(x_tilde)
+            self.hidden_state.append(self.tanh_lay.forward(u_t))
+            o = self.fc_out.forward(self.hidden_state[-1])
+            self.output[time] = self.sig_lay.forward(o)
+
+        return self.output
+
+    def backward(self, error_tensor):
+
+        self.gradient_weights_val = np.zeros_like(self.fc_hidden.weights)
+        self.gradient_weights_hy_val = np.zeros_like(self.fc_out.weights)
+        output_error = np.zeros((self.batch_size, self.input_size))
+        error_h = np.zeros((1, self.hidden_size))
+
+        # bp through time
+        for revtime in reversed(range(error_tensor.shape[0])):
+            x_tilde = np.concatenate((self.input_tensor[revtime, :], self.hidden_state[revtime]), axis=None).reshape(1, -1)
+            u = self.fc_hidden.forward(x_tilde)
+            h = self.tanh_lay.forward(u)
+            o = self.fc_out.forward(h)
+            output = self.sig_lay.forward(o)
+            # backward
+            grad = self.sig_lay.backward(error_tensor[revtime, :])
+            grad = self.fc_out.backward(grad) + error_h
+            self.gradient_weights_hy_val += self.fc_out.gradient_weights
+            grad = self.tanh_lay.backward(grad)
+            grad = self.fc_hidden.backward(grad)
+            self.gradient_weights_val += self.fc_hidden.gradient_weights
+            output_error[revtime, :] = grad[:, :self.input_size]
+            error_h = grad[:, self.input_size:]
+
+        if self.optimizer:
+            self.fc_hidden.weights = self.optimizer.calculate_update(
+                self.fc_hidden.weights, self.gradient_weights_val)
+            self.fc_out.weights = self.optimizer.calculate_update(
+                self.fc_out.weights, self.gradient_weights_hy_val)
+
+        self.weights = self.fc_hidden.weights
+        self.weights_hy = self.fc_out.weights
+
+        return output_error
 
     @property
     def memorize(self):
-        return self.memval
-    
+        return self.memorize_val
+
     @memorize.setter
-    def memorize(self, newval):
-        self.memval = newval
-    
+    def memorize(self, memorize):
+        self.memorize_val = memorize
+
     @property
     def weights(self):
         return self.fc_hidden.weights
-    
+
     @weights.setter
-    def weights(self, newval):
-        self.fc_hidden.weights = newval
+    def weights(self, weights):
+        self.fc_hidden.weights = weights
 
     @property
     def gradient_weights(self):
-        return self.fc_hidden.weights
-    
+        return self.gradient_weights_val
+
     @gradient_weights.setter
-    def gradient_weights(self, newval):
-        self.fc_hidden.gradient_weights = newval
-        self.fc_out.gradient_weights = newval
+    def gradient_weights(self, new_weights):
+        self.fc_hidden._gradient_weights = new_weights
+
+    @property
+    def optimizer(self):
+        return self.optimizer_val
+
+    @optimizer.setter
+    def optimizer(self, optimizer):
+        self.optimizer_val = copy.deepcopy(optimizer)
 
